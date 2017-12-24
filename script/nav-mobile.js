a0e7c08123e5914232003210286943c4d2d981a5
luke.vo <lukevo1101@gmail.com>
2017-12-24 12:49:12 +0700

    Almost done

--- script/nav-mobile.js
+++ script/nav-mobile.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
     $('.icon').click(function () {
+
         $('.icon').toggleClass('active');
-        $('.nav-mb-wrapper').toggle();
-        $('header').toggleClass('show-background');
-        $('nav-mb').addClass('show-background');
-        $('header').toggleClass('animated fadeInDown')
+        $('.nav-mb-wrapper').toggle('nav-mb-wrapper');
     });
     $('.nav-mb').on('click', 'li', function () {
-        $('header').hide();
+        $('.icon').toggleClass('active');
     });
 });
